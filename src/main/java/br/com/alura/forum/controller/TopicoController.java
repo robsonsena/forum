ef4fb314c1680ef1ab8f19e4309e9b04b3e9be40
RobsonSena <robsonsena@gmail.com>
2019-10-19 22:41:05 -0300

    Suegger e cache

--- src/main/java/br/com/alura/forum/controller/TopicoController.java
+++ src/main/java/br/com/alura/forum/controller/TopicoController.java
@@ -8,6 +8,8 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -46,6 +48,7 @@ public class TopicoController {
 	
 	
 	@GetMapping
+	@Cacheable(value = "listaDeTopicos")
 	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso,
 			@PageableDefault(sort = "id", direction = Direction.DESC, page = 0,size = 10) Pageable paginacao) {
 		
@@ -61,6 +64,8 @@ public class TopicoController {
 
 	@PostMapping
 	@Transactional
+	//evict - invalidar cache
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)  
 	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
@@ -81,6 +86,8 @@ public class TopicoController {
 	
 	@PutMapping ("/{id}")
 	@Transactional
+	//evict - invalidar cache
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)  
 	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id,  @RequestBody @Valid AtualizacaoTopicoFrom form){
 		Optional<Topico> optional = topicoRepository.findById(id);
 		if (optional.isPresent()) {
@@ -92,6 +99,8 @@ public class TopicoController {
 	
 	@Transactional
 	@DeleteMapping("/{id}")
+	//evict - invalidar cache
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)  
 	public ResponseEntity<?> remover(@PathVariable Long id){
 		Optional<Topico> optional = topicoRepository.findById(id);
 		if (optional.isPresent()) {
